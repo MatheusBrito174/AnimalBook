69d271c58f86217663738303eb889cf50e82f9ef
Matheus <matheusbrito174@gmail.com>
2022-04-30 17:50:10 -0300

    Criando HomeModule e configurando estilos e rotas

---
+++ animalbook/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
